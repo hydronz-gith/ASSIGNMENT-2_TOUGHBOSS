dbb25d36a7ce07e0c3f500bf2199b0d70960c2ea
hydronz-gith <hydronz15@gmail.com>
2026-04-03 00:47:31 +1100

    WR02/03
    Dialogue Branch system

---
+++ Assignment-2/Assets/Brad Files/DudeChat.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+public class DudeChat : MonoBehaviour
+{
+    public NPCConversation myConversation;
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+        ConversationManager.Instance.StartConversation(myConversation);
+        }
+    }
+}
