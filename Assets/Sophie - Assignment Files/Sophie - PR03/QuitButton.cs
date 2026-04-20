480b18dfe97b01e620a5ecb4d4b90d5b548281a8
105924231 <105924231@student.swin.edu.au>
2026-04-20 10:20:47 +1000

    minor pr03 task changes

--- Assets/Sophie - Assignment Files/Sophie - PR03/QuitButton.cs
+++ Assets/Sophie - Assignment Files/Sophie - PR03/QuitButton.cs
@@ -4,10 +4,6 @@ public class QuitButton : MonoBehaviour
 {
     public void QuitGame()
     {
-        #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false; // stops play mode in editor
-        #else
         Application.Quit(); // quits the built game
-        #endif
     }
 }
